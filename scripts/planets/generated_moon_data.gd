68a331648ba477a1422bee6d06f2260e82d5286d
Brenden Brayshaw <brayshawbrenden@gmail.com>
2026-09-21 15:36:38 +0200

    Add moons and asteroid field simulation

---
+++ scripts/planets/generated_moon_data.gd
@@ -0,0 +1,10 @@
+class_name GeneratedMoonData
+extends Resource
+
+@export var id: String = ""
+@export var display_name: String = ""
+@export var radius: float = 0.0
+@export var mass: float = 0.0
+@export var orbital_distance: float = 0.0
+@export var orbital_angle: float = 0.0
+@export var orbital_period: float = 1.0
