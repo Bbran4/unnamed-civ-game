22b53ac1514a8ffa885febd6b6f431c75615141c
Brenden Brayshaw <brayshawbrenden@gmail.com>
2026-09-21 15:37:49 +0200

    Add station orbital data

--- scripts/economy/generated_station_data.gd
+++ scripts/economy/generated_station_data.gd
@@ -7,3 +7,6 @@ extends Resource
 @export var faction: FactionData
 @export var planet_id: String = ""
 @export var population: int = 0
+@export var orbital_distance: float = 0.0
+@export var orbital_angle: float = 0.0
+@export var orbital_period: float = 1.0
