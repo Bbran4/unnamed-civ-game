2ed5342db03c03c7454fb1f7718fbc02d14ba3e4
Brenden Brayshaw <brayshawbrenden@gmail.com>
2026-09-21 10:14:10 +0200

    Fix control selector player call

--- scripts/ui/control_style_selector.gd
+++ scripts/ui/control_style_selector.gd
@@ -2,7 +2,7 @@ extends CanvasLayer
 
 @onready var rotation_keys_button: Button = $Panel/Container/RotationKeysButton
 @onready var mouse_aim_button: Button = $Panel/Container/MouseAimButton
-@onready var player_ship: CharacterBody2D = get_tree().get_first_node_in_group("player_ship") as CharacterBody2D
+@onready var player_ship: Node = get_tree().get_first_node_in_group("player_ship")
 
 func _ready() -> void:
 	rotation_keys_button.pressed.connect(_select_rotation_keys)
@@ -20,11 +20,11 @@ func _select_mouse_aim() -> void:
 	_update_buttons()
 
 func _apply_to_player() -> void:
-	player_ship = get_tree().get_first_node_in_group("player_ship") as CharacterBody2D
+	player_ship = get_tree().get_first_node_in_group("player_ship") as Node
 	if not is_instance_valid(player_ship):
 		return
 
-	player_ship.set_control_style(WorldState.control_style as player_ship.ControlStyle)
+	player_ship.call("set_control_style", WorldState.control_style)
 
 func _update_buttons() -> void:
 	rotation_keys_button.button_pressed = WorldState.control_style == 0
