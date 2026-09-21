70486eda2ef829b0e03dbf93a9504ba679a4f0aa
Brenden Brayshaw <brayshawbrenden@gmail.com>
2026-09-21 14:03:36 +0200

    Add generated system data model

---
+++ scripts/world/generated_system_data.gd
@@ -0,0 +1,8 @@
+class_name GeneratedSystemData
+extends Resource
+
+@export var id: String = ""
+@export var display_name: String = ""
+@export var seed_value: int = 0
+@export var planets: Array[GeneratedPlanetData] = []
+@export var stations: Array[GeneratedStationData] = []
