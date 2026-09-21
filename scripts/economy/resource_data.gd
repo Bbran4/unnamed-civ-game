6a3c4ae08cce1ba4fa89c4e3de41f7a74fe56b4f
Brenden Brayshaw <brayshawbrenden@gmail.com>
2026-09-21 14:03:54 +0200

    Use shared commodity base for resources

--- scripts/economy/resource_data.gd
+++ scripts/economy/resource_data.gd
@@ -1,9 +1,2 @@
 class_name ResourceData
-extends Resource
-
-@export var id: String = ""
-@export var display_name: String = ""
-@export_multiline var description: String = ""
-@export var base_value: float = 0.0
-@export var mass: float = 0.0
-@export var category: String = ""
+extends CommodityData
