0406f283994c65fa83c04d6e79df60071bfb38c2
Brenden Brayshaw <brayshawbrenden@gmail.com>
2026-09-21 08:46:30 +0200

    fix forward and backward movement direction

--- scripts/player/player_ship.gd
+++ scripts/player/player_ship.gd
@@ -22,7 +22,7 @@ func _physics_process(delta: float) -> void:
 	var movement_input: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var forward_direction: Vector2 = Vector2.RIGHT.rotated(rotation)
 	var right_direction: Vector2 = forward_direction.rotated(PI * 0.5)
-	var movement_direction: Vector2 = (forward_direction * movement_input.y) + (right_direction * movement_input.x)
+	var movement_direction: Vector2 = (forward_direction * -movement_input.y) + (right_direction * movement_input.x)
 	var target_velocity: Vector2 = movement_direction * move_speed
 
 	velocity = velocity.move_toward(target_velocity, acceleration * delta)
