9e32d0b6c90738d3a3dbcb0ad2cd014da5e7ed8b
Brenden Brayshaw <brayshawbrenden@gmail.com>
2026-09-21 14:02:56 +0200

    Add generated station data model

---
+++ scripts/economy/generated_station_data.gd
@@ -0,0 +1,9 @@
+class_name GeneratedStationData
+extends Resource
+
+@export var id: String = ""
+@export var display_name: String = ""
+@export var station_type: StationTypeData
+@export var faction: FactionData
+@export var planet_id: String = ""
+@export var population: int = 0
