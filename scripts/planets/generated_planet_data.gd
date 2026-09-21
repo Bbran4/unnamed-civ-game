bbf7a1a5c7003e21a8641266e0c26a62df14f49b
Brenden Brayshaw <brayshawbrenden@gmail.com>
2026-09-21 15:37:46 +0200

    Add planetary mass and moon data

--- scripts/planets/generated_planet_data.gd
+++ scripts/planets/generated_planet_data.gd
@@ -5,6 +5,7 @@ extends Resource
 @export var display_name: String = ""
 @export var planet_type: PlanetTypeData
 @export var radius: float = 0.0
+@export var mass: float = 0.0
 @export var gravity: float = 0.0
 @export var temperature: float = 0.0
 @export var atmosphere: String = ""
@@ -13,3 +14,4 @@ extends Resource
 @export var orbital_distance: float = 0.0
 @export var orbital_angle: float = 0.0
 @export var orbital_period: float = 1.0
+@export var moons: Array[GeneratedMoonData] = []
