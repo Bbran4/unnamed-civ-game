4b805f05d27cfe3416687b92318252e550f16f16
Brenden Brayshaw <brayshawbrenden@gmail.com>
2026-09-21 15:15:40 +0200

    Add generated star data to systems

--- scripts/world/generated_system_data.gd
+++ scripts/world/generated_system_data.gd
@@ -4,5 +4,9 @@ extends Resource
 @export var id: String = ""
 @export var display_name: String = ""
 @export var seed_value: int = 0
+@export var star: StarData
+@export var star_radius: float = 0.0
+@export var star_mass: float = 1.0
+@export var star_temperature: float = 0.0
 @export var planets: Array[GeneratedPlanetData] = []
 @export var stations: Array[GeneratedStationData] = []
