5ff216e9e7850bbec24db9c378b8340c69974fa3
Brenden Brayshaw <brayshawbrenden@gmail.com>
2026-09-21 09:02:53 +0200

    change ship controls to Star Valor style

--- scripts/player/player_ship.gd
+++ scripts/player/player_ship.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
-@export var thrust_acceleration: float = 650.0
-@export var strafe_acceleration: float = 450.0
+@export var forward_acceleration: float = 650.0
+@export var reverse_acceleration: float = 350.0
+@export var turn_speed: float = 3.5
 @export var braking_acceleration: float = 850.0
 @export var max_speed: float = 700.0
 @export var max_hull: float = 100.0
@@ -17,24 +18,21 @@ func _ready() -> void:
 	current_hull = max_hull
 
 func _physics_process(delta: float) -> void:
-	var aim_direction: Vector2 = get_global_mouse_position() - global_position
-	if aim_direction.length_squared() > 0.0:
-		rotation = aim_direction.angle()
+	var turn_input: float = Input.get_axis("move_left", "move_right")
+
+	if turn_input != 0.0:
+		rotation += turn_input * turn_speed * delta
 
 	var forward_direction: Vector2 = Vector2.RIGHT.rotated(rotation)
-	var right_direction: Vector2 = forward_direction.rotated(PI * 0.5)
 
 	if Input.is_action_pressed("move_up"):
-		velocity += forward_direction * thrust_acceleration * delta
+		velocity += forward_direction * forward_acceleration * delta
 
 	if Input.is_action_pressed("move_down"):
-		velocity = velocity.move_toward(Vector2.ZERO, braking_acceleration * delta)
-
-	if Input.is_action_pressed("move_left"):
-		velocity += -right_direction * strafe_acceleration * delta
-
-	if Input.is_action_pressed("move_right"):
-		velocity += right_direction * strafe_acceleration * delta
+		velocity = velocity.move_toward(
+			Vector2.ZERO,
+			braking_acceleration * delta
+		)
 
 	if velocity.length() > max_speed:
 		velocity = velocity.normalized() * max_speed
@@ -54,8 +52,13 @@ func _fire_primary() -> void:
 	var projectile_instance: Node = projectile_scene.instantiate()
 	if projectile_instance is Node2D:
 		var projectile_2d: Node2D = projectile_instance
+		var aim_direction: Vector2 = get_global_mouse_position() - weapon_muzzle.global_position
+
+		if aim_direction.length_squared() <= 0.0:
+			return
+
 		projectile_2d.global_position = weapon_muzzle.global_position
-		projectile_2d.global_rotation = global_rotation
+		projectile_2d.global_rotation = aim_direction.angle()
 		projectile_2d.set("owner_group", "player_projectile")
 		get_tree().current_scene.add_child(projectile_2d)
 		fire_cooldown_remaining = primary_fire_cooldown
