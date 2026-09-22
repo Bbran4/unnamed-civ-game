3827efd1aabedd4a033e2eaf988280fcc8ab3f4a
Brenden Brayshaw <brayshawbrenden@gmail.com>
2026-09-22 07:22:58 +0200

    Add faction relationship data model

--- scripts/factions/faction_data.gd
+++ scripts/factions/faction_data.gd
@@ -7,3 +7,13 @@ extends Resource
 @export var faction_category: String = "civilian"
 @export var preferred_station_types: Array[StationTypeData] = []
 @export var preferred_goods: Array[GoodData] = []
+@export var relations: Dictionary[String, int] = {}
+
+func get_relation_to(other_faction_id: String) -> int:
+	return relations.get(other_faction_id, 0)
+
+func is_hostile_to(other_faction_id: String) -> bool:
+	return get_relation_to(other_faction_id) <= -50
+
+func is_friendly_to(other_faction_id: String) -> bool:
+	return get_relation_to(other_faction_id) >= 50
