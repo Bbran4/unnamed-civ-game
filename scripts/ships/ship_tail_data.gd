b267633178a78570ef312874d18b673bb26607bd
Brenden Brayshaw <brayshawbrenden@gmail.com>
2026-09-22 07:25:43 +0200

    Add ship tail data foundation

---
+++ scripts/ships/ship_tail_data.gd
@@ -0,0 +1,9 @@
+class_name ShipTailData
+extends Resource
+
+@export var id: String = ""
+@export var display_name: String = ""
+@export_multiline var description: String = ""
+@export var max_speed: float = 300.0
+@export var acceleration: float = 100.0
+@export var boost_multiplier: float = 1.5
