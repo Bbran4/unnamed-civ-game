16b60da5b26ff03486b635426ac3750ce54e1861
Brenden Brayshaw <brayshawbrenden@gmail.com>
2026-09-22 22:31:47 +0200

    Render resource deposits

--- scripts/world/world_renderer.gd
+++ scripts/world/world_renderer.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 const TILE_SIZE := 12.0
 const MAP_ORIGIN := Vector2(256.0, 12.0)
+const RESOURCE_MARKER_RADIUS := 2.5
 
 func _ready() -> void:
 	queue_redraw()
@@ -12,6 +13,7 @@ func _draw() -> void:
 	var width := int(world_data.get("width", 0))
 	var height := int(world_data.get("height", 0))
 	var terrain: Array = world_data.get("terrain", [])
+	var resources: Array = world_data.get("resources", [])
 
 	if width <= 0 or height <= 0 or terrain.size() != width * height:
 		return
@@ -25,6 +27,12 @@ func _draw() -> void:
 				Vector2.ONE * TILE_SIZE
 			)
 			draw_rect(rect, _terrain_color(terrain_type))
+
+			if resources.size() == width * height:
+				var resource_type := int(resources[index])
+				if resource_type != WorldGenerator.RESOURCE_NONE:
+					var center := rect.get_center()
+					draw_circle(center, RESOURCE_MARKER_RADIUS, _resource_color(resource_type))
 
 	draw_string(
 		ThemeDB.fallback_font,
@@ -47,5 +55,18 @@ func _terrain_color(terrain_type: int) -> Color:
 			return Color("777777")
 		WorldGenerator.TERRAIN_DESERT:
 			return Color("c99d5b")
+		_:
+			return Color("ff00ff")
+
+func _resource_color(resource_type: int) -> Color:
+	match resource_type:
+		WorldGenerator.RESOURCE_FOOD:
+			return Color("f4d35e")
+		WorldGenerator.RESOURCE_WOOD:
+			return Color("6b4226")
+		WorldGenerator.RESOURCE_STONE:
+			return Color("d0d0d0")
+		WorldGenerator.RESOURCE_FISH:
+			return Color("e8f1f2")
 		_:
 			return Color("ff00ff")
