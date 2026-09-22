ce4fc712acaec439a16eb75435896b3f3ddffc4f
Brenden Brayshaw <brayshawbrenden@gmail.com>
2026-09-22 07:20:07 +0200

    Add faction categories

--- scripts/factions/faction_data.gd
+++ scripts/factions/faction_data.gd
@@ -4,5 +4,6 @@ extends Resource
 @export var id: String = ""
 @export var display_name: String = ""
 @export_multiline var description: String = ""
+@export var faction_category: String = "civilian"
 @export var preferred_station_types: Array[StationTypeData] = []
 @export var preferred_goods: Array[GoodData] = []
