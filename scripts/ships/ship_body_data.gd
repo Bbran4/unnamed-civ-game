f9ff82066495db70955ec51079af43b5e25f9774
Brenden Brayshaw <brayshawbrenden@gmail.com>
2026-09-22 07:25:27 +0200

    Add ship body data foundation

---
+++ scripts/ships/ship_body_data.gd
@@ -0,0 +1,9 @@
+class_name ShipBodyData
+extends Resource
+
+@export var id: String = ""
+@export var display_name: String = ""
+@export_multiline var description: String = ""
+@export var hull_points: int = 100
+@export var armor: int = 100
+@export var module_slots: int = 2
